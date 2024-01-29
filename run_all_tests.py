2200c10ba09e09ec8d24fb615f33a9d901d74c26
flowkraft <virgil.trasca@gmail.com>
2024-01-29 14:06:50 +0000

    ----

---
+++ run_all_tests.py
@@ -0,0 +1,12 @@
+import subprocess
+import os
+
+# Base directory for your projects and tests
+base_dir = 'design-patterns-python'
+
+# List of test files
+test_files = ['tests/gof_creational_abstract_factory.py']
+
+for test_file in test_files:
+    print(f"Running tests for {test_file}")
+    subprocess.run(['pytest', os.path.join(base_dir, test_file)])
