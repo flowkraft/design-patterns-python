7d3070cbc66518f1a1f5af395cfacc94327dc334
sourcekraft <virgil.trasca@gmail.com>
2024-02-07 18:44:54 +0100

    update to gof builder pattern

--- patterns/gof_creational_builder.py
+++ patterns/gof_creational_builder.py
@@ -1,94 +1,80 @@
 # Entity: Product
 class Pizza:
     def __init__(self):
-        self.dough = ""
-        self.sauce = ""
-        self.toppings = []
+        self._dough = ""
+        self._sauce = ""
+        self._toppings = []
 
     def set_dough(self, dough):
-        self.dough = dough
+        self._dough = dough
 
     def set_sauce(self, sauce):
-        self.sauce = sauce
+        self._sauce = sauce
 
     def add_topping(self, topping):
-        self.toppings.append(topping)
+        self._toppings.append(topping)
 
     def __str__(self):
-        return f"Pizza with {self.dough} dough, {self.sauce} sauce, toppings: {', '.join(self.toppings) if self.toppings else 'no toppings'}"
+        return f"Pizza with {self._dough} dough, {self._sauce} sauce, toppings: {', '.join(self._toppings) if self._toppings else 'no toppings'}"
 
 # Entity: Builder Interface
 class PizzaBuilder:
-    def prepare_dough(self, dough):
-        pass
-
-    def add_sauce(self, sauce):
-        pass
-
-    def add_toppings(self, toppings):
-        pass
-
-    def build(self):
-        pass
-
-# Entity: ConcreteBuilder
-class PizzaBaseBuilder(PizzaBuilder):
     def __init__(self):
+        self.default_dough = None
+        self.default_sauce = None
+        self.default_toppings = None
         self.pizza = Pizza()
 
-    def prepare_dough(self, dough):
+    def prepare_dough(self, dough=None):
+        if dough is None:
+            dough = self.default_dough
         self.pizza.set_dough(dough)
         return self
 
-    def add_sauce(self, sauce):
+    def add_sauce(self, sauce=None):
+        if sauce is None:
+            sauce = self.default_sauce
         self.pizza.set_sauce(sauce)
         return self
 
-    def add_toppings(self, toppings):
+    def add_toppings(self, toppings=None):
+        if toppings is None:
+            toppings = self.default_toppings
         for topping in toppings:
             self.pizza.add_topping(topping)
         return self
 
     def build(self):
-        return self.pizza
+        pizza = self.pizza
+        self.pizza = Pizza()  # Reset for next build
+        return pizza
 
 # Entity: ConcreteBuilder
-class MargheritaPizzaBuilder(PizzaBaseBuilder):
+class MargheritaPizzaBuilder(PizzaBuilder):
     def __init__(self):
         super().__init__()
-
-    def prepare_dough(self):
-        super().prepare_dough("Thin Crust")
-        return self
-
-    def add_sauce(self):
-        super().add_sauce("Tomato")
-        return self
-
-    def add_toppings(self):
-        super().add_toppings(["Mozzarella", "Basil"])
-        return self
+        self.default_dough = "Thin Crust"
+        self.default_sauce = "Tomato"
+        self.default_toppings = ["Mozzarella", "Basil"]
 
 # Entity: Director
 class PizzaDirector:
     def __init__(self, builder):
+        assert isinstance(builder, PizzaBuilder), 'Builder must be an instance of PizzaBuilder'
         self._builder = builder
 
     def make_pizza(self, dough=None, sauce=None, toppings=None):
-        if dough:
-            self._builder.prepare_dough(dough)
-        if sauce:
-            self._builder.add_sauce(sauce)
-        if toppings:
-            self._builder.add_toppings(toppings)
+        self._builder.prepare_dough(dough)
+        self._builder.add_sauce(sauce)
+        self._builder.add_toppings(toppings)
         return self._builder.build()
 
 # Client code example
 if __name__ == "__main__":
     # Using the base builder for a custom pizza
-    customPizzaBuilder = PizzaBaseBuilder()
+    customPizzaBuilder = PizzaBuilder()
     director = PizzaDirector(customPizzaBuilder)
-    customPizza = director.make_pizza().prepare_dough("Whole Wheat").add_sauce("BBQ").add_toppings(["Chicken", "Onion", "Pepper"]).build()
+    customPizza = director.make_pizza(dough = "Whole Wheat", sauce= "BBQ", toppings = ["Chicken", "Onion", "Pepper"])
     print("Custom Pizza:", customPizza)
 
     # Using the MargheritaPizzaBuilder
--- tests/gof_creational_builder_test.py
+++ tests/gof_creational_builder_test.py
@@ -1,25 +1,25 @@
 import unittest
-from patterns.gof_creational_builder import PizzaBaseBuilder, MargheritaPizzaBuilder, PizzaDirector
+from patterns.gof_creational_builder import PizzaBuilder, MargheritaPizzaBuilder, PizzaDirector
 
-class TestPizzaBaseBuilder(unittest.TestCase):
+class TestPizzaBuilder(unittest.TestCase):
     def setUp(self):
-        self.builder = PizzaBaseBuilder()
+        self.builder = PizzaBuilder()
         self.director = PizzaDirector(self.builder)
-        self.pizza = self.director.make_pizza().prepare_dough("Whole Wheat").add_sauce("BBQ").add_toppings(["Chicken", "Onion", "Pepper"]).build()
+        self.pizza = self.director.make_pizza("Whole Wheat", "BBQ", ["Chicken", "Onion", "Pepper"])
 
     def test_dough_type_base_builder(self):
-        self.assertEqual(self.pizza.dough, "Whole Wheat", "Dough type should be 'Whole Wheat'")
+        self.assertEqual(self.pizza._dough, "Whole Wheat", "Dough type should be 'Whole Wheat'")
 
     def test_sauce_type_base_builder(self):
-        self.assertEqual(self.pizza.sauce, "BBQ", "Sauce should be 'BBQ'")
+        self.assertEqual(self.pizza._sauce, "BBQ", "Sauce should be 'BBQ'")
 
     def test_toppings_base_builder(self):
         expected_toppings = ["Chicken", "Onion", "Pepper"]
         for topping in expected_toppings:
-            self.assertIn(topping, self.pizza.toppings, f"Pizza should contain '{topping}'")
+            self.assertIn(topping, self.pizza._toppings, f"Pizza should contain '{topping}'")
 
     def test_number_of_toppings_base_builder(self):
-        self.assertEqual(len(self.pizza.toppings), 3, "Pizza should have exactly 3 toppings")
+        self.assertEqual(len(self.pizza._toppings), 3, "Pizza should have exactly 3 toppings")
 
 class TestMargheritaPizzaBuilder(unittest.TestCase):
     def setUp(self):
@@ -28,18 +28,18 @@ class TestMargheritaPizzaBuilder(unittest.TestCase):
         self.pizza = self.director.make_pizza()
 
     def test_dough_type_margherita_builder(self):
-        self.assertEqual(self.pizza.dough, "Thin Crust", "Dough type should be 'Thin Crust'")
+        self.assertEqual(self.pizza._dough, "Thin Crust", "Dough type should be 'Thin Crust'")
 
     def test_sauce_type_margherita_builder(self):
-        self.assertEqual(self.pizza.sauce, "Tomato", "Sauce should be 'Tomato'")
+        self.assertEqual(self.pizza._sauce, "Tomato", "Sauce should be 'Tomato'")
 
     def test_toppings_margherita_builder(self):
         expected_toppings = ["Mozzarella", "Basil"]
         for topping in expected_toppings:
-            self.assertIn(topping, self.pizza.toppings, f"Pizza should contain '{topping}'")
+            self.assertIn(topping, self.pizza._toppings, f"Pizza should contain '{topping}'")
 
     def test_number_of_toppings_margherita_builder(self):
-        self.assertEqual(len(self.pizza.toppings), 2, "Margherita Pizza should have exactly 2 toppings")
+        self.assertEqual(len(self.pizza._toppings), 2, "Margherita Pizza should have exactly 2 toppings")
 
 if __name__ == '__main__':
     unittest.main()
